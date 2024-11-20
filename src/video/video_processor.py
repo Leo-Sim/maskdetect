2022cc8b7deccc0ca543148c85d90e33dcad922e
heesu <heesu12200@gmail.com>
2024-11-20 14:43:36 -0600

    apply new trained yolo version

--- src/video/video_processor.py
+++ src/video/video_processor.py
@@ -13,11 +13,11 @@ sys.path.append(os.path.abspath('../config'))
 from preprocessor import Preprocessor
 from config import Config
 
-video_path = '../../data/video/test.mp4'
+video_path = '../../data/video/tes1t.mp4'
 cap = cv2.VideoCapture(video_path)
 
 # Load YOLO
-yolo_model = YOLO('../yolo/runs/train/exp_with_new_label8/weights/best.pt')  # Use a YOLO model (e.g., yolov5s.pt for YOLOv5)
+yolo_model = YOLO('../yolo/runs/train/yolo_train5/weights/best.pt')
 
 mask_model_path = '../train/model.pth'
 mask_model = torch.load(mask_model_path)
@@ -25,62 +25,60 @@ mask_model = torch.load(mask_model_path)
 config = Config()
 image_size = config.get_image_size()
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+mask_model.to(device)
+
 # Read frames
 while cap.isOpened():
     ret, frame = cap.read()  # Read a frame
     if not ret:
         break
 
-    # put frames in yolo
-    results = yolo_model(frame, conf=0.5)  # Pass the frame to the YOLO model
-    # print('result : ', results.boxes)
-    # Draw bounding boxes around detected objects
+    # Pass the frame to the YOLO model
+    results = yolo_model(frame, conf=0.5)
+
     for box in results[0].boxes:
 
-        x1, y1, x2, y2 = map(int, box.xyxy[0])  # Bounding box coordinates
-        class_id = int(box.cls[0])  # Detected class ID
-        print('class id : ',class_id)
-        confidence = float(box.conf[0])  # Confidence score
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+        class_id = int(box.cls[0])
+
+        confidence = float(box.conf[0])
 
         if class_id == 0:
             cropped_frame = frame[y1:y2, x1:x2]
-            # cropped_frame = cropped_frame.transpose((2, 0, 1))
-            # tensor = torch.tensor(cropped_frame)
+            tensor = torch.tensor(cropped_frame)
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 0), 2)
+            cv2.putText(frame, f"{confidence: .2f}", (x1, y1 - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)  # Label
 
             image = Image.fromarray(cropped_frame)
+
 
             transforms = Preprocessor(image_size=image_size).get_transforms()
 
             tensor = transforms(image)
             tensor = tensor.unsqueeze(0)
 
+            # move tensor to gpu
+            tensor = tensor.to(device)
+
+
             mask_output = mask_model(tensor)
-            detection_result = mask_output.tolist()
-            # print('mask_output ' , mask_output.tolist())
+            device = next(mask_model.parameters()).device
+            print(f"The model is running on: {device}")
 
-            prop_not_mask = detection_result[0][0]
-            prop_mask = detection_result[0][1]
-
-            label = 'No mask' if prop_not_mask > prop_mask else 'Mask'
-
-
+            # if prop_not_mask < prop_mask:
+            #     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Draw rectangle
+            #     cv2.putText(frame, f"{''} {prop_mask:.2f}", (x1, y1 - 10),
+            #                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)  # Label
 
 
-        # Get label name from class ID
-        # label = results[0].names[class_id]  # Convert class_id to label
-
-        # Draw bounding box and label on the frame
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Draw rectangle
-            cv2.putText(frame, f"{label} {confidence:.2f}", (x1, y1 - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)  # Label
-
-    # Display the frame with YOLO detections
     cv2.imshow('YOLO Object Detection', frame)
 
-    # Exit the loop if 'q' is pressed
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release resources
 cap.release()
 cv2.destroyAllWindows()
--- src/video/video_requester.py
+++ src/video/video_requester.py
@@ -1,11 +1,11 @@
 
 import yt_dlp
 
-video_url = 'https://www.youtube.com/watch?v=eurAOZuzTag'
+video_url = 'https://www.youtube.com/watch?v=wxmmrwYT2SE'
 
 ydl_opts = {
     'outtmpl': 'video/%(title)s.%(ext)s',
-    'format': 'bestvideo'
+    'format': 'best[height<=480]'
 }
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=eurAOZuzTag'])
+    ydl.download(['https://www.youtube.com/watch?v=wxmmrwYT2SE'])
